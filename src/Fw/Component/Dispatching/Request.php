087abfcb0cd1f18f831017287d817902aa1b1e82
Laura Quintana Sais <laura.quintana.sais@gmail.com>
2015-06-22 21:44:49 +0200

    Trying composition for request

--- src/Fw/Component/Dispatching/Request.php
+++ src/Fw/Component/Dispatching/Request.php
@@ -2,3 +2,40 @@
 
 namespace Fw\Component\Dispatching;
 
+use ArrayAccess;
+
+class Request implements ArrayAccess {
+
+    private $container = array();
+
+
+    public function __construct($get, $post, $session) {
+        $this->container = array(
+            "get"   => $get,
+            "post"   => $post,
+            "session" => $session,
+        );
+    }
+
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->container[$offset]);
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->container[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+}
+
