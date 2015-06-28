ca8604fbd3dc3a0afc574184f5f2697b9dac0cc3
Laura Quintana Sais <laura.quintana.sais@gmail.com>
2015-06-28 22:56:52 +0200

    Added database to controller interface

--- src/Fw/Component/Controllers/Controller.php
+++ src/Fw/Component/Controllers/Controller.php
@@ -2,7 +2,8 @@
 
 namespace Fw\Component\Controllers;
 use Fw\Component\Dispatching\HttpRequest;
+use Fw\Component\Databases\Database;
 
 interface Controller {
-    public function __invoke(HttpRequest $request);
+    public function __invoke(HttpRequest $request,  Database $database);
 }
